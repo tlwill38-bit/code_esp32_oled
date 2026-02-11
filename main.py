377561f6ec2e82ecb4ecc8f9a975d7e6d3ebcd27
tlwill38-bit <tlwill38@asu.edu>
2026-02-11 13:07:31 -0700

    Implement state-based OLED display updates
    Added a loop to print state and update OLED display based on state values.

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
 import my_oled
+import time
+state = 0
+while True:
+  print(state)
+  time.sleep(1)
+  state = state+1
+    if state > 4:
+      State = 0;
+    if state == 0:
+      my_oled.print_text("test",0,0)
+    if state == 1:
+      my_oled.print_text("something else",1,0)
+      
+    #Line
+    if state == 2:
+      fill(0)
+      my_oled.oled.line(0,0,10,10,1)
+      oled.show()
+      
+    #Recatangle
+    if state == 3:
+      fill(0)
+      my_oled.graphics.fill_rect(0,0,10,10,1)
+      oled.show()
+
